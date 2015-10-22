da89a0279804d178bce8120178a5ec97980a3106
平田裕哉 <yuya.h0623@gmail.com>
2015-10-22 21:15:48 +0900

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019104808) do
+ActiveRecord::Schema.define(version: 20151022095900) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "copy",       limit: 255
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
