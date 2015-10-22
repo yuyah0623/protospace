30708a2bdfe00bf0d1c28fe3b7b80d3653752b1f
平田裕哉 <yuya.h0623@gmail.com>
2015-10-23 01:05:47 +0900

    create a create action and set the strong parameter

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,4 +7,14 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
     @prototype.captured_images.build
   end
+
+  def create
+    current_user.prototypes.create(create_params)
+    redirect_to root_path
+  end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :copy, :concept, captured_images_attributes: [:name])
+  end
 end
