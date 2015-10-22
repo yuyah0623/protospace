a3acbfaf87da560a0fa5a2921f876462c4787f2d
平田裕哉 <yuya.h0623@gmail.com>
2015-10-22 11:55:12 +0900

    fix before_filter to before_action and fix strong parameter notation in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :name, :profile, :work, :group, :avatar) }
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :profile, :work, :group, :avatar]
   end
 end
