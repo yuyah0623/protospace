56b00dafdd40a3e185ddc92e9984ce6f458d8c2d
平田裕哉 <yuya.h0623@gmail.com>
2015-10-23 23:32:26 +0900

    add validation in prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ActiveRecord::Base
   has_many :captured_images
   accepts_nested_attributes_for :captured_images
+  validates :title, :copy, :concept, :captured_images, presence: true
   acts_as_taggable_on :tags
 end
