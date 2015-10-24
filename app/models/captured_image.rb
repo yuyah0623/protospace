55738fb53fa5bb5182f0b26c03648ff466f336e0
平田裕哉 <yuya.h0623@gmail.com>
2015-10-24 15:18:20 +0900

    add spaces captured_image model and user model

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -1,4 +1,5 @@
 class CapturedImage < ActiveRecord::Base
   belongs_to :prototype
+
   mount_uploader :name, CapturedImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :prototypes
+
   mount_uploader :avatar, CapturedImageUploader
 end
