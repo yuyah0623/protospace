1b8795c8af9a1bd72d2014c760664c161b33dca0
平田裕哉 <yuya.h0623@gmail.com>
2015-10-24 15:13:40 +0900

    add space in prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,9 @@
 class Prototype < ActiveRecord::Base
   has_many :captured_images
-  accepts_nested_attributes_for :captured_images
+
   validates :title, :copy, :concept, :captured_images, presence: true
+
+  accepts_nested_attributes_for :captured_images
+
   acts_as_taggable_on :tags
 end
