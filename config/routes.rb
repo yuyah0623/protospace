930283fd68248a73802b68a40ceac30446a35b15
平田裕哉 <yuya.h0623@gmail.com>
2015-11-04 01:29:19 +0900

    Add routing related to likes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :likes, only: [:create, :destroy]
   resources :prototypes do
     resources :comments, only: [:new, :create]
   end
